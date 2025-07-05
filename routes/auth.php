4279884ae290c689110c41fe1652b0f8fa2f34a5
Anas <apple@APPLEs-MacBook-Pro.local>
2025-07-05 15:57:52 +0400

    Resolve-CI Error-3

--- routes/auth.php
+++ routes/auth.php
@@ -36,23 +36,25 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    // Route::get('verify-email', EmailVerificationPromptController::class)
-    //     ->name('verification.notice');
 
-    // Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-    //     ->middleware(['signed', 'throttle:6,1'])
-    //     ->name('verification.verify');
 
-    // Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-    //     ->middleware('throttle:6,1')
-    //     ->name('verification.send');
+    Route::get('verify-email', EmailVerificationPromptController::class)
+        ->name('verification.notice');
 
-    // Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-    //     ->name('password.confirm');
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
 
-    // Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+        ->middleware('throttle:6,1')
+        ->name('verification.send');
 
-    // Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+        ->name('password.confirm');
+
+    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+
+    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/admin/tickets', [AdminController::class, 'index']);
     Route::get('/admin/tickets/{type}/{id}', [AdminController::class, 'show']);
     Route::post('/admin/tickets/{type}/{id}/note', [AdminController::class, 'note']);
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
 });
 
